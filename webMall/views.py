41b0d3d895faf0f82d572acfc8119280361f4a79
Sabbir Hossain <m.sabbirhossain308@gmail.com>
2020-07-07 13:28:57 +0600

    Django Forms

--- webMall/views.py
+++ webMall/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+#Django From Import
+from .forms import ContactForm
+
 def home_page(request):
     context = {
         "title" : "Home Page",
@@ -16,14 +19,21 @@ def about_page(request):
     return render(request, "home_page.html", context)
 
 def contact_page(request):
+    contact_form = ContactForm(request.POST or None)
+
     context = {
         "title" : "Contact Page",
-        "content" : "Welcome To Contact Page"
+        "content" : "Welcome To Contact Page",
+        "form" : contact_form
     }
-    if request.method == "POST":
-        print(request.POST.get('fullname'))
-        print(request.POST.get('email'))
-        print(request.POST.get('content'))
+
+    if contact_form.is_valid():
+        print(contact_form.cleaned_data)
+
+    # if request.method == "POST":
+    #     print(request.POST.get('fullname'))
+    #     print(request.POST.get('email'))
+    #     print(request.POST.get('content'))
     return render(request, "contact/view.html", context)
 
 
