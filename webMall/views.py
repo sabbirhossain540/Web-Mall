ac60caf99e59a0c6c0dbcd2b9f1e81208a82cd89
Sabbir Hossain <m.sabbirhossain308@gmail.com>
2020-07-07 14:00:22 +0600

    User Login Process

--- webMall/views.py
+++ webMall/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 #Django From Import
-from .forms import ContactForm
+from .forms import ContactForm, LoginForm
 
 def home_page(request):
     context = {
@@ -36,6 +37,37 @@ def contact_page(request):
     #     print(request.POST.get('content'))
     return render(request, "contact/view.html", context)
 
+
+def login_page(request):
+    login_form = LoginForm(request.POST or None)
+    
+    context = {
+        "form": login_form
+    }
+    print(request.user.is_authenticated)
+    if login_form.is_valid():
+        print(login_form.cleaned_data)
+        username = login_form.cleaned_data.get("username")
+        password = login_form.cleaned_data.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            #context['form'] = LoginForm()
+            return redirect("/login")
+        else:
+            print("error")
+    return render(request, "auth/login.html", context)
+
+
+
+
+
+def register_page(request):
+    login_form = LoginForm(request.POST or None)
+    
+    if login_form.is_valid():
+        print(login_form.cleaned_data)
+    return render(request, "auth/register.html", {})
 
 
 def home_page_old(request):
