9e488dd86d33d24e5bfd62324cac9c069a031df0
Sabbir Hossain <m.sabbirhossain308@gmail.com>
2020-07-09 13:15:33 +0600

    Django Admin

--- products/models.py
+++ products/models.py
@@ -7,4 +7,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=20, default=39.99)
 
+    def __str__(self):
+        return self.title
+
 
