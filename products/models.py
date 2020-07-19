8f1b4d1f19ea32314df4fb4d5a3118a85e87fcec
Sabbir Hossain <m.sabbirhossain308@gmail.com>
2020-07-19 12:00:45 +0600

    Template Filtering

--- products/models.py
+++ products/models.py
@@ -57,6 +57,7 @@ class Product(models.Model):
     image = models.ImageField(upload_to=upload_image_path, null=True, blank=True)  #Advance Way including custom file name
     featured = models.BooleanField(default=False)
     active = models.BooleanField(default=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     objects = ProductManager()
 
