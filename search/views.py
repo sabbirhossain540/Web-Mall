9cfec39f9e9ae3df5f33eaaa9f99d8b53bf8a1ff
Sabbir Hossain <m.sabbirhossain308@gmail.com>
2020-07-23 09:00:30 +0600

    Display the query to the user

--- search/views.py
+++ search/views.py
@@ -5,7 +5,12 @@ from products.models import Product
 # Create your views here.
 
 class SearchProductView(ListView):
-    template_name = "product/product_list.html"
+    template_name = "search/view.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(SearchProductView, self).get_context_data(*args, **kwargs)
+        context['query'] = self.request.GET.get('q')
+        return context
 
     def get_queryset(self, *args, **kwargs):
         request = self.request
